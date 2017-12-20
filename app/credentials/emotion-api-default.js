1c40c1134e249d8d49df7a130c9a04c18b535308
mphonic <lloydlyson@gmail.com>
2017-12-20 00:16:55 -0500

    Add default emotion-api file

---
+++ app/credentials/emotion-api-default.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+    EMOTION_API_URL: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?',
+    EMOTION_API_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+});
